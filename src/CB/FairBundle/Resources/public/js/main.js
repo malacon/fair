289dc3b7d3c8c68a0aaa2b3f0a478702fc6147e0
Craig <craig.d.baker@gmail.com>
2013-07-30 15:44:14 -0500

    Fix ajax?

--- src/CB/FairBundle/Resources/public/js/main.js
+++ src/CB/FairBundle/Resources/public/js/main.js
@@ -1,3 +1,6 @@
+if (typeof console == "undefined") {
+    this.console = {log: function() {}};
+}
 $(function() {
     $('.content-item .help').popover({trigger: 'hover', html: true, container: 'body'});
     $('.btn-booth').button();
