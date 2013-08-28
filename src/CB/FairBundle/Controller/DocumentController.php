f0c202691a6f72939e7760f3de995718d4f5ec7a
Craig <craig.d.baker@gmail.com>
2013-08-28 12:03:23 -0500

    doc upload fix

--- src/CB/FairBundle/Controller/DocumentController.php
+++ src/CB/FairBundle/Controller/DocumentController.php
@@ -199,7 +199,7 @@ class DocumentController extends Controller
                 $family->setExpiresAt($date2);
                 $family->setPassword($encoder->encodePassword($row['Student ID'], $family->getSalt()));
             } else if (!in_array($row['Student Name'], $userList)) {
-                $family = $em->getRepository('UserBundle:Family')->find($userList[$row['Student Name']]);
+                $family = $em->getRepository('UserBundle:Family')->find($userList[$row['Student Name']]);;
             } else {
                 $family = $families[$row['Student ID']];
             }
